90dcc978e85069be098d3f4e6169aeba35f2484b
Franc Sander van Schaick <fs.vanschaick@gmail.com>
2014-05-06 16:47:08 +0200

    files om te compilen op bbb

---
+++ device_tree_overlay/README.md
@@ -0,0 +1,5 @@
+In originals de onbewerkte device tree sources
+
+In de main map de device tree sources.
+
+In de map binaries de .dtbo bestanden
---
+++ device_tree_overlay/mountcapes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#verwijder hekjes voor instellen device trees
+
+cat $SLOTS
+
+#UART van PRU
+#echo BB-BONE-PRU-UART-CDS > $SLOTS
+
+
+#PRU test
+#echo BB-BONE-PRU-ADC-CDS > $SLOTS
+
+#ADC pinnen pru
+#echo BB-BONE-PRU-ADC-CDS > $SLOTS
+
+#uart4
+#echo BB-UART4-CDS > $SLOTS
+
+#SPI1
+#echo BB-SPIDEV1 > $SLOTS
+
+#SPI1 geedit
+#echo BB-SPI1-CDS > $SLOTS
+
+
+cat $SLOTS
