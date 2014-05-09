c8674880c245129fc8d1e7c49689ffbe501e2f45
Franc Sander van Schaick <fs.vanschaick@gmail.com>
2014-05-09 15:33:29 +0200

    bla

---
+++ src/spi_talk.h
@@ -0,0 +1,16 @@
+/*
+ * spi_talk.h
+ *
+ *  Created on: May 9, 2014
+ *      Author: sander
+ */
+
+#ifndef SPI_TALK_H_
+#define SPI_TALK_H_
+
+#define FT800_SPEED 1000000 //1MHz
+#define FT800_DFORMAT SPI_MODE_0 //mode 0
+
+
+
+#endif /* SPI_TALK_H_ */
