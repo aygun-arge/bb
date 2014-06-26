58957fd3bfe7f2746597eb3424377c69e55fd3f2
Franc Sander van Schaick <fs.vanschaick@gmail.com>
2014-06-26 23:13:41 +0200

    started adding doxy

--- src/spi.c
+++ src/spi.c
@@ -23,6 +23,15 @@
 //static const char *device = "/dev/spidev1.0"
 //static uint8_t mode = SPI_MODE_0;
 
+/**
+ *  \brief Brief
+ *  
+ *  \param [in] fd Parameter_Description
+ *  \param [in] cmd Parameter_Description
+ *  \return Return_Description
+ *  
+ *  \details Details
+ */
 void command_write(int fd, uint8_t cmd )
 {
     printf("Writing CMD :%#02x \n", cmd);
@@ -44,7 +53,17 @@ void command_write(int fd, uint8_t cmd )
     	pabort("can't send spi message");
     }
 }
-    
+ 
+/**
+ *  \brief Writes 32 bits of data to a specified adress by SPI.
+ *  
+ *  \param [in] fd Filepointer, should be the SPI Device one.
+ *  \param [in] Addr, 22 bit address where data will be written to
+ *  \param [in] Data, 4 bytes of data to write to the address of Addr
+ *  \return Return_Description
+ *  
+ *  \details Details
+ */ 
 void memory_write32(int fd, uint32_t Addr, uint32_t Data)
 {
 	static uint8_t mask = 0xFF;
@@ -53,10 +72,11 @@ void memory_write32(int fd, uint32_t Addr, uint32_t Data)
 
     uint8_t addrMSB, addrMID, addrLSB, datapartMSB, datapartMID1, datapartMID2, datapartLSB;
     
-    addrLSB = Addr & mask;
-    addrMID = (Addr >> 8)&mask;
-    addrMSB = ((Addr >> 16)&partmask)|com_mode;
+    addrLSB = Addr & mask;						/*!< Detailed description after the member */
+    addrMID = (Addr >> 8)&mask; 				/*!< Detailed description after the member */
+    addrMSB = ((Addr >> 16)&partmask)|com_mode;	/*!< Detailed description after the member */
     
+	
     datapartLSB  =  Data & mask;
     datapartMID1 = (Data >> 8) & mask;
     datapartMID2 = (Data >> 16) & mask;
@@ -64,9 +84,6 @@ void memory_write32(int fd, uint32_t Addr, uint32_t Data)
     
     const uint8_t memwrs32[] = {addrMSB, addrMID, addrLSB, datapartLSB, datapartMID1,  datapartMID2, datapartMSB};
     
-
-    //const uint8_t memwrs32[] = {(((Addr >> 16)&partmask)|com_mode), ((Addr >> 8)&mask), (Addr & mask), (Data & mask), ((Data >> 8) & mask), ((Data >> 16) & mask), ((Data >> 24) & mask)};
-
     struct spi_ioc_transfer memwrite32 =
     {
         .tx_buf = (unsigned long)memwrs32,
@@ -84,18 +101,31 @@ void memory_write32(int fd, uint32_t Addr, uint32_t Data)
     }
 }
 
+/**
+ *  \brief Function which writes specified data bytes with the size of two bytes to a specified address over the SPI bus.
+ *  
+ *  \param [in] fd, File pointer of the writing operation, in this case it should be the the SPI device pointer.
+ *  \param [in] Addr, A 22 bits address where data should be written to.
+ *  \param [in] Data, Data with the size of two bytes should be inserted here.
+ *  
+ *  \details A function which is used to write fout bytes of data to an 22 bits long address over a spi device.
+ */
 void memory_write16(int fd, uint32_t Addr, uint16_t Data)
 {
-    int ret;
-	static uint8_t mask = 0xFF;
-    static uint8_t partmask = 0x3F;
-    static uint8_t com_mode = 0x80; //voor mem_write
+	static uint8_t mask = 0xFF;		/*!< A mask which is used to tear the 22 bits adress into single bytes. */
+    static uint8_t partmask = 0x3F; /*!< A partial mask which is used to add the com_mode to the address */
+    static uint8_t com_mode = 0x80; /*!< com_mode houses the specific 2 MSB (10) for the memory_write function for the FT800 */ 
     uint8_t addrMSB, addrMID, addrLSB, datapartLSB, datapartMSB ;
 
-    addrLSB = Addr & mask;
-    addrMID = (Addr >> 8)&mask;
-    addrMSB = ((Addr >> 16)&partmask)|com_mode;
+    addrLSB = Addr & mask;						/*!< AddrLSB is the least significant part of the inserted Address */
+    addrMID = (Addr >> 8)&mask; 				/*!< AddrMID is the middle part of the inserted Address */
+    addrMSB = ((Addr >> 16)&partmask)|com_mode;	/*!< AddrMSB is the most significant part of the inserted Address */
 
+	/*! @brief: Because the data consists out of two bytes it must be broking into single bytes.
+	 * 			For the FT800 data has to be sent with a little endian format and MSB values.
+	 *			This means the data has to be broken into multiple bytes with a standard formatting
+	 */
+	
     datapartLSB  =  Data & mask;
     datapartMSB = (Data >> 8) & mask;
 
@@ -118,17 +148,25 @@ void memory_write16(int fd, uint32_t Addr, uint16_t Data)
     }
 }
 
+/**
+ *  \brief Function which writes a specified data byte to a specified address over the SPI bus.
+ *  
+ *  \param [in] fd, File pointer of the writing operation, in this case it should be the the SPI device pointer.
+ *  \param [in] Addr, A 22 bits address where data should be written to.
+ *  \param [in] Data, Data with the size of one byte should be inserted here.
+ *  
+ *  \details A function which is used to write one single byte of data to an 22 bits long address over a spi device.
+ */
 void memory_write8(int fd, uint32_t Addr, uint8_t Data)
 {
-    int ret;
-	static uint8_t mask = 0xFF;
-    static uint8_t partmask = 0x3F;
-    static uint8_t com_mode = 0x80; //voor mem_write
+	static uint8_t mask = 0xFF;		/*!< A mask which is used to tear the 22 bits adress into single bytes. */
+    static uint8_t partmask = 0x3F; /*!< A partial mask which is used to add the com_mode to the address */
+    static uint8_t com_mode = 0x80; /*!< com_mode houses the specific 2 MSB (10) for the memory_write function for the FT800 */ 
     uint8_t addrMSB, addrMID, addrLSB;
 
-    addrLSB = Addr & mask;
-    addrMID = (Addr >> 8)&mask;
-    addrMSB = ((Addr >> 16)&partmask)|com_mode;
+    addrLSB = Addr & mask;						/*!< AddrLSB is the least significant part of the inserted Address */
+    addrMID = (Addr >> 8)&mask; 				/*!< AddrMID is the middle part of the inserted Address */
+    addrMSB = ((Addr >> 16)&partmask)|com_mode;	/*!< AddrMSB is the most significant part of the inserted Address */
 
     const uint8_t memwrs8[] = {addrMSB, addrMID, addrLSB, Data};
 
